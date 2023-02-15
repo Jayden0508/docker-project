9f676576c752207b185968d9a4152e3895799d5f
Jayden <jaysenai05@hotmail.com>
2023-02-15 15:13:51 +0100

    html

---
+++ site/select.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav> 
+<ul> 
+   <li>
+    <a href="show.php">show zonder waarde</a>
+   </li>
+   <li>
+    <a href="show.php?waarde1=honderd"> show met1 waarde</a>
+  </li>
+   <li>
+    <a href="show.php?waarde1=tweehonderd&waarde2=Drie"> show met twee waardes </a>
+   </li>
+    <li>
+    <a href="show.php?waarde1=Snoop%20Dog&waarde2=Dr%20Dre"> Snoop Dog en Dr Dre</a>
+   </li>
+
+</ul>
+</nav>
+</body>
+</html>
